98e24b60695f421f3f822e4c8ac682fd8688b3e5
Suchea <bsuchea@gmail.com>
2024-05-19 16:10:40 +0700

    update student

--- API/app/Http/Requests/Student/StoreStudentRequest.php
+++ API/app/Http/Requests/Student/StoreStudentRequest.php
@@ -24,66 +24,46 @@ class StoreStudentRequest extends FormRequest
     {
         return [
 
-            'code' => [
-                'required',
-                'max:20',
-                Rule::unique('students', 'code')->where('school_id', auth()->user()->school_id)
-            ],
+            'code'              => ['required', 'max:20', 'unique:students'],
+            'first_name'        => ['required', 'max:50'],
+            'last_name'         => ['required', 'max:50'],
+            'first_name_latin'  => ['required', 'max:50'],
+            'last_name_latin'   => ['required', 'max:50'],
+            'gender'            => ['required', Rule::in([1, 2])],
+            'dob'               => ['required', 'date'],
+            'from'              => ['nullable', 'max:255'],
+            'photo_path'        => ['nullable', 'image'],
+            'other'             => ['nullable', 'max:255'],
+            'student_status'    => ['nullable', Rule::in([1, 2, 3, 4])],
+            'status'            => ['nullable', Rule::in([1, 2])],
+            'phone'             => ['nullable', 'max:50'],
 
-            'name' => [
-                'required',
-                'max:100'
-            ],
+            'village_birth'     => ['required', 'max:100'],
+            'commune_birth'     => ['required', 'max:100'],
+            'district_birth'    => ['required', 'max:100'],
+            'province_birth'    => ['required', 'max:100'],
 
-            'name_latin' => [
-                'nullable',
-                'max:100'
-            ],
+            'village'           => ['required', 'max:100'],
+            'commune'           => ['required', 'max:100'],
+            'district'          => ['required', 'max:100'],
+            'province'          => ['required', 'max:100'],
 
-            'sex' => [
-                'required',
-                Rule::in([1, 2])
-            ],
+            'd_first_name'      => ['nullable', 'max:50'],
+            'd_last_name'       => ['nullable', 'max:50'],
+            'd_job'             => ['nullable', 'max:50'],
+            'd_phone_number'    => ['nullable', 'max:50'],
 
-            'dob' => [
-                'nullable',
-                'date',
-            ],
+            'm_first_name'      => ['nullable', 'max:50'],
+            'm_last_name'       => ['nullable', 'max:50'],
+            'm_job'             => ['nullable', 'max:50'],
+            'm_phone_number'    => ['nullable', 'max:50'],
 
-            'photo_path' => [
-                'nullable',
-                'image'
-            ],
-
-            'pob' => [
-                'nullable',
-                'max:255'
-            ],
-
-            'address' => [
-                'nullable',
-                'max:255'
-            ],
-
-            'dad_name' => [
-                'nullable',
-                'max:50'
-            ],
-
-            'dad_phone' => [
-                'nullable',
-                'max:50'
-            ],
-
-            'mom_name' => [
-                'nullable',
-                'max:50'
-            ],
-
-            'mom_phone' => [
-                'nullable',
-                'max:50'
-            ],
+            'g_first_name'      => ['nullable', 'max:50'],
+            'g_last_name'       => ['nullable', 'max:50'],
+            'g_phone_number'    => ['nullable', 'max:50'],
+            'g_job'             => ['nullable', 'max:100'],
+            'g_gender'          => ['nullable', Rule::in([1, 2])],
+            'g_detail'          => ['nullable', 'max:255'],
 
         ];
     }
--- API/database/migrations/2023_06_05_035925_create_students_table.php
+++ API/database/migrations/2023_06_05_035925_create_students_table.php
@@ -12,20 +12,48 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->foreignId('school_id')->index();
-            $table->string('code', 20)->nullable()->index();
-            $table->string('name', 100);
-            $table->string('name_latin', 100);
-            $table->tinyInteger('sex')->nullable();
-            $table->date('dob')->nullable();
-            $table->string('photo_path')->nullable();
-            $table->string('pob')->nullable();
-            $table->string('address')->nullable();
-            $table->string('dad_name', 50)->nullable();
-            $table->string('dad_phone', 50)->nullable();
-            $table->string('mom_name', 50)->nullable();
-            $table->string('mom_phone', 50)->nullable();
+            $table->uuid('id')->primary();//
+            $table->string('code', 20)->unique()->nullable();
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('first_name_latin', 50);
+            $table->string('last_name_latin', 50);
+            $table->tinyInteger('gender');
+            $table->date('dob');
+            $table->string('village_birth', 100);
+            $table->string('commune_birth', 100);
+            $table->string('district_birth', 100);
+            $table->string('province_birth', 100);
+            //
+            $table->string('d_first_name', 50)->nullable();
+            $table->string('d_last_name', 50)->nullable();
+            $table->string('d_job', 50)->nullable();
+            $table->string('d_phone_number', 50)->nullable();
+            //
+            $table->string('m_first_name', 50)->nullable();
+            $table->string('m_last_name', 50)->nullable();
+            $table->string('m_job', 50)->nullable();
+            $table->string('m_phone_number', 50)->nullable();
+            //
+            $table->string('village', 100);
+            $table->string('commune', 100);
+            $table->string('district', 100);
+            $table->string('province', 100);
+            $table->string('phone', 50)->nullable();
+            $table->tinyInteger('student_status')->nullable();
+            
+            // 1 orphan dad, 2 orphan mom, 3 orphan dad and mom, 4 disabled ពិការភាព
+            $table->string('from')->nullable();
+            $table->string('photo_path', 100)->nullable();
+            $table->string('other')->nullable();
+            $table->tinyInteger('status')->default(1);
+            // guardian
+            $table->string('g_first_name', 50)->nullable();
+            $table->string('g_last_name', 50)->nullable();
+            $table->string('g_phone_number', 50)->nullable();
+            $table->string('g_job', 50)->nullable();
+            $table->tinyInteger('g_gender')->nullable();
+            $table->text('g_detail',)->nullable();
             $table->uuid('created_by')->nullable();
             $table->uuid('updated_by')->nullable();
             $table->softDeletes();
